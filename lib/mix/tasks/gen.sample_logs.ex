0d8488cc1470b82a44a98d6078c6d98224f1a88d
Stephen Ball <sdball@gmail.com>
2024-02-05 11:50:54 -0500

    Add a task to allow generating sample logs
    e.g.
    
    ```
    $ mix gen.sample_logs --log-size 10MB
    $ mix gen.sample_logs --log-size 1GB
    ```
    
    It's not _fast_ but it does generate real looking logs.
    
    If we really wanted to optimize for speed we could overload the `yes`
    command or write some Rust.

---
+++ lib/mix/tasks/gen.sample_logs.ex
@@ -0,0 +1,63 @@
+defmodule Mix.Tasks.Gen.SampleLogs do
+  use Mix.Task
+
+  @shortdoc "Generate sample log files up to a declared file size."
+
+  def run(args) do
+    options =
+      OptionParser.parse(args,
+        switches: [log_size: :string]
+      )
+
+    case options do
+      {[log_size: log_size_arg], _, _} when not is_nil(log_size_arg) ->
+        log_size = parse_log_size(log_size_arg)
+        generate_sample_logs(log_size, log_size_arg)
+
+      _ ->
+        Mix.shell().error(
+          "Invalid or missing options. Usage: mix gen.sample_logs --log-size 100MB"
+        )
+    end
+  end
+
+  defp parse_log_size(size_string) do
+    {size, unit} = Integer.parse(size_string)
+
+    case unit do
+      "MB" ->
+        size * 1_000_000
+
+      "GB" ->
+        size * 1_000_000_000
+    end
+  end
+
+  defp generate_sample_logs(target_size_bytes, target_size) do
+    File.mkdir_p!("sample_logs")
+    log_path = Path.join(["sample_logs", "sample.#{target_size}.log"])
+
+    File.open!(log_path, [:write], fn file ->
+      Enum.reduce_while(Stream.repeatedly(&generate_log_entry/0), 0, fn entry, acc ->
+        if acc > target_size_bytes do
+          {:halt, acc}
+        else
+          IO.write(file, entry)
+          {:cont, acc + byte_size(entry)}
+        end
+      end)
+    end)
+
+    Mix.shell().info("Generated sample log file at #{log_path}")
+  end
+
+  defp generate_log_entry do
+    timestamp = DateTime.utc_now() |> DateTime.to_string()
+    hostname = "server#{self() |> :erlang.pid_to_list() |> :erlang.list_to_bitstring()}"
+    pid = Enum.random(1111..9999)
+    message = "Sample log message from #{pid}"
+    process = Enum.random(["xyzzy", "frotz", "nitfol", "plugh"])
+
+    "#{timestamp} #{hostname} #{process}[#{pid}]: #{message}\n"
+  end
+end
