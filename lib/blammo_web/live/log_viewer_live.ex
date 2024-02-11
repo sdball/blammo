963a2c887a891c347f9f662257d169d34d0aac96
Stephen Ball <sdball@gmail.com>
2024-02-10 21:28:20 -0500

    Add support for other nodes to the web UX
    Now the web UX presents a select menu of peer nodes and logs can be
    read from any selected node.

--- lib/blammo_web/live/log_viewer_live.ex
+++ lib/blammo_web/live/log_viewer_live.ex
@@ -1,23 +1,46 @@
 defmodule BlammoWeb.LogViewerLive do
   use BlammoWeb, :live_view
 
+  alias Blammo.OtherServers
+
   def mount(_params, _session, socket) do
+    if connected?(socket) do
+      Phoenix.PubSub.subscribe(Blammo.PubSub, "servers")
+    end
+
     {:ok,
      assign(socket,
+       servers: OtherServers.list(),
        files: get_files(),
+       server: nil,
+       file: nil,
        log_content: "",
        lines: 25,
        filter: nil,
-       file: nil,
        error: nil,
        busy: false
      ), layout: false}
   end
 
-  def handle_event("tail_log", %{"file" => file, "lines" => lines, "filter" => filter}, socket) do
+  def handle_event("fetch_server_files", %{"server" => server}, socket) do
+    server_files = OtherServers.files(server |> String.to_existing_atom())
+    {:noreply, assign(socket, files: server_files, file: nil, log_content: "", server: server)}
+  end
+
+  def handle_event(
+        "tail_log",
+        %{"server" => server, "file" => file, "lines" => lines, "filter" => filter},
+        socket
+      ) do
     GenServer.cast(
       self(),
-      {:tail_log, %{"file" => file, "lines" => lines, "filter" => filter}}
+      {:tail_log,
+       %{
+         "server" => server,
+         "file" => file,
+         "lines" => lines,
+         "filter" => filter
+       }}
     )
 
     {:noreply, assign(socket, busy: true, log_content: "Reading … … …")}
@@ -25,50 +48,67 @@ defmodule BlammoWeb.LogViewerLive do
 
   def handle_event("tail_log", _params, socket), do: {:noreply, socket}
 
-  def handle_cast({:tail_log, %{"file" => file, "lines" => lines, "filter" => filter}}, socket) do
+  def handle_cast(
+        {:tail_log,
+         %{
+           "server" => server,
+           "file" => file,
+           "lines" => lines,
+           "filter" => filter
+         }},
+        socket
+      ) do
+    socket =
+      assign(socket,
+        server: server,
+        file: file,
+        lines: lines,
+        filter: filter,
+        busy: false,
+        error: nil,
+        log_content: ""
+      )
+
     case Integer.parse(lines) do
       {lines, _} when lines <= 0 ->
-        {:noreply, assign(socket, busy: false)}
+        {:noreply,
+         assign(socket,
+           error: "lines must be greater than zero"
+         )}
 
       {lines, _} ->
-        case tail_log(file, lines, filter) do
+        case tail_log(server, file, lines, filter) do
           {:ok, ""} ->
             {:noreply,
              assign(socket,
-               error: nil,
-               log_content: "--- NO RESULTS ---",
-               lines: lines,
-               filter: filter,
-               file: file,
-               busy: false
+               log_content: "--- NO RESULTS ---"
              )}
 
           {:ok, content} ->
-            {:noreply,
-             assign(socket,
-               error: nil,
-               log_content: content,
-               lines: lines,
-               filter: filter,
-               file: file,
-               busy: false
-             )}
+            if String.valid?(content) do
+              {:noreply,
+               assign(socket,
+                 log_content: content
+               )}
+            else
+              {:noreply,
+               assign(socket,
+                 log_content: "--- NON PRINTABLE FILE CONTENT --"
+               )}
+            end
 
           {:error, reason} ->
-            {:noreply,
-             assign(socket,
-               log_content: "",
-               error: reason,
-               lines: lines,
-               filter: filter,
-               file: file,
-               busy: false
-             )}
+            {:noreply, assign(socket, error: reason)}
         end
 
       _error ->
-        {:noreply, assign(socket, busy: false, log_content: "")}
+        {:noreply, socket}
     end
+  end
+
+  def handle_info({:server, nodename, pid}, socket) do
+    updated_servers = Map.put(socket.assigns.servers, nodename, pid)
+    {:noreply, assign(socket, servers: updated_servers)}
   end
 
   def handle_info(_any, socket) do
@@ -79,15 +119,7 @@ defmodule BlammoWeb.LogViewerLive do
     Blammo.LogConsumer.log_files()
   end
 
-  defp tail_log(file, lines, filter) do
-    opts = %{
-      filename: file,
-      lines: lines,
-      filter: filter
-    }
-
-    with {:ok, options} <- Blammo.LogConsumer.Options.build(opts) do
-      Blammo.LogConsumer.consume_filter_first(options)
-    end
+  defp tail_log(server, file, lines, filter) do
+    OtherServers.tail_log(server |> String.to_existing_atom(), file, filter, lines)
   end
 end
